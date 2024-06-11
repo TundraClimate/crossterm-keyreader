d2cb09fdbb7382ac9b7fad3e15f1e8d67aa71f06
TundraClimate <tundra.tundere@gmail.com>
2024-06-12 00:40:32 +0900

    📝docs: add docs on lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,26 @@
 #![warn(missing_docs)]
 #![warn(rustdoc::missing_crate_level_docs)]
+
+//! Crossterm KeyReader is a library crate for handling key input events using the crossterm crate in Rust.
+//!
+//! This crate provides an interface to easily capture and process key inputs in terminal applications.
+//!
+//! ## Features
+//!
+//! - Simple API: KeyReader offers a simple and intuitive API for processing key input events.
+//! - Asynchronous Support: Supports asynchronous programming for efficient input handling.
+//! - Cross-Platform: Built on top of the Crossterm crate, it works on Windows, Linux, and macOS.
+//!
+//! ## Installation
+//!
+//! Add the following dependency to your Cargo.toml:
+//! ```text
+//! [dependencies]
+//! crossterm-keyreader = "0.1"
+//! ```
+//!
+//! ## Example
+//!
+//! ```
+//! // Example usage here.
+//! ```
