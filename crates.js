5baf9a04a7e9dba18d15459f5d9807baa040c5b1
TundraClimate <TundraClimate@users.noreply.github.com>
2024-06-11 15:41:04 +0000

    deploy: d2cb09fdbb7382ac9b7fad3e15f1e8d67aa71f06

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["crossterm_keyreader"];
+window.ALL_CRATES = ["bitflags","cfg_if","crossterm","crossterm_keyreader","libc","lock_api","log","mio","parking_lot","parking_lot_core","pin_project_lite","scopeguard","signal_hook","signal_hook_mio","signal_hook_registry","smallvec","socket2","tokio"];
