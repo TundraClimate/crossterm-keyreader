f9dcf50013b166807c1e995f45fb3fd1949cbe36
TundraClimate <TundraClimate@users.noreply.github.com>
2024-06-12 03:08:18 +0000

    deploy: e3e6894006924ce47c078c1cb430152821d6c88d

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["bitflags","cfg_if","crossterm","crossterm_keyreader","libc","lock_api","log","mio","parking_lot","parking_lot_core","pin_project_lite","scopeguard","signal_hook","signal_hook_mio","signal_hook_registry","smallvec","socket2","tokio"];
+window.ALL_CRATES = ["bitflags","cfg_if","crossterm","crossterm_keyreader","libc","lock_api","log","mio","num_cpus","parking_lot","parking_lot_core","pin_project_lite","proc_macro2","quote","scopeguard","signal_hook","signal_hook_mio","signal_hook_registry","smallvec","socket2","syn","tokio","tokio_macros","unicode_ident"];
